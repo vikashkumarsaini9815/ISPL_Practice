d3346e6c078141376bca74787c498b586351ac7e
vikash saini <vickykartoos1598@gmail.com>
2022-11-24 15:37:11 +0530

    change json file format

--- ISPL_project/urls.py
+++ ISPL_project/urls.py
@@ -17,7 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 
 
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('ISPL_app.urls'))
+    
 ]
