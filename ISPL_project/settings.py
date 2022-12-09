5672208cd9f392b0e017b270c681fe8aadce072c
vikash saini <vickykartoos1598@gmail.com>
2022-12-09 12:36:33 +0530

    change requirment.txt

--- ISPL_project/settings.py
+++ ISPL_project/settings.py
@@ -182,7 +182,7 @@ MEDIA_URL = '/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '')
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
