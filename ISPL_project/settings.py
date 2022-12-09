ff855dadda852b736b0c50ab679800d35c577992
vikash saini <vickykartoos1598@gmail.com>
2022-12-09 12:21:28 +0530

    change database postgresql

--- ISPL_project/settings.py
+++ ISPL_project/settings.py
@@ -118,12 +118,30 @@ WSGI_APPLICATION = 'ISPL_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ISPL_database',
+        'USER': 'ISPL_user',
+        'PASSWORD': 'isplpass@123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
+
+
+
+
 
 
 # Password validation
